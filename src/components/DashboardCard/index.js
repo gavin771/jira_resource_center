5dbc2c1e56061fcf8535637b871a50d8e3f8a63b
gavin771 <gavin.samuels10@gmail.com>
2017-08-16 23:39:00 -0500

    converted dashboard card to functional component

--- src/components/DashboardCard/index.js
+++ src/components/DashboardCard/index.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Card, Image, Divider, Button } from 'semantic-ui-react'
 
-class DashboardCard extends Component {
-  render () {
-    return (
-      <Card raised href={this.props.link}>
-        <Card.Content>
-          <Image size='small' src={this.props.image} centered />
-          <Divider />
-          <Card.Header className='center aligned'>
-            {this.props.header}
-          </Card.Header>
-          <Card.Description>
-            {this.props.details}
-          </Card.Description>
-        </Card.Content>
-        {this.props.displayExtra &&
-          <Card.Content extra>
-            <div className='ui two buttons'>
-              <Button basic color='green'>
-                View
-              </Button>
-              <Button basic color='blue'>
-                Download
-              </Button>
-            </div>
-          </Card.Content>}
-      </Card>
-    )
-  }
+const DashboardCard = props => {
+  return (
+    <Card raised href={this.props.link}>
+      <Card.Content>
+        <Image size='small' src={this.props.image} centered />
+        <Divider />
+        <Card.Header className='center aligned'>
+          {this.props.header}
+        </Card.Header>
+        <Card.Description>
+          {this.props.details}
+        </Card.Description>
+      </Card.Content>
+      {this.props.displayExtra &&
+        <Card.Content extra>
+          <div className='ui two buttons'>
+            <Button basic color='green'>
+              View
+            </Button>
+            <Button basic color='blue'>
+              Download
+            </Button>
+          </div>
+        </Card.Content>}
+    </Card>
+  )
 }
 
 export default DashboardCard
