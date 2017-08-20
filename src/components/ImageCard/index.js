dcfc72dc9b6661a9872cf82efde801bb8022ca5c
gavin771 <gavin.samuels10@gmail.com>
2017-08-20 03:13:42 -0500

    added image link to imagecard

--- src/components/ImageCard/index.js
+++ src/components/ImageCard/index.js
@@ -5,9 +5,9 @@ const ImageCard = props => {
   return (
     <Card raised href='#'>
       <Image alt={props.alt} src={props.link} />
-      <Card.Content extra>
-        <div className='centered ui one buttons'>
-          <Button basic color='green'>
+      <Card.Content extra className='ui center aligned '>
+        <div className='ui one buttons'>
+          <Button basic color='green' as='a' href={props.link} target='_blank'>
             Download PNG
           </Button>
         </div>
